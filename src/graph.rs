4316a65ecc21a92a0bcb50a6bf8204cd2ea53e61
Artem Kudyakov <t3m8ch@yandex.ru>
2025-10-13 02:32:01 +0400

    feat(task 1): Implement graph

---
+++ src/graph.rs
@@ -0,0 +1,271 @@
+use std::collections::{HashMap, HashSet};
+
+use derivative::Derivative;
+use thiserror::Error;
+
+// Сюда в будущем будут добавляться свойства рёбер/дуг
+#[derive(Derivative)]
+#[derivative(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Edge {
+    pub node_id: usize,
+}
+
+#[derive(Clone, Debug)]
+pub struct BaseGraph<T> {
+    next_id: usize,
+    nodes: HashMap<usize, T>,
+    edges: HashMap<usize, HashSet<Edge>>,
+}
+
+impl<T> BaseGraph<T> {
+    pub fn new() -> Self {
+        BaseGraph {
+            next_id: 0,
+            nodes: HashMap::new(),
+            edges: HashMap::new(),
+        }
+    }
+
+    pub fn add_node(&mut self, value: T) -> usize {
+        let id = self.next_id;
+        self.next_id += 1;
+        self.nodes.insert(id, value);
+        self.edges.insert(id, HashSet::new());
+        id
+    }
+
+    fn remove_node(&mut self, id: usize) {
+        self.nodes.remove(&id);
+        self.edges.remove(&id);
+        for edge in self.edges.values_mut() {
+            edge.retain(|e| e.node_id != id);
+        }
+    }
+
+    fn add_edge(&mut self, from: usize, to: usize) {
+        self.edges
+            .entry(from)
+            .or_insert_with(HashSet::new)
+            .insert(Edge { node_id: to });
+    }
+
+    fn remove_edge(&mut self, from: usize, to: usize) {
+        if let Some(edges) = self.edges.get_mut(&from) {
+            edges.remove(&Edge { node_id: to });
+        }
+    }
+
+    fn has_node(&self, id: usize) -> bool {
+        self.nodes.contains_key(&id)
+    }
+
+    fn has_edge(&self, from: usize, to: usize) -> bool {
+        if let Some(edges) = self.edges.get(&from) {
+            edges.contains(&Edge { node_id: to })
+        } else {
+            false
+        }
+    }
+
+    fn from_edges(&self, id: usize) -> HashSet<usize> {
+        self.edges
+            .get(&id)
+            .unwrap_or(&HashSet::new())
+            .iter()
+            .map(|to| to.node_id)
+            .collect()
+    }
+
+    fn to_edges(&self, id: usize) -> HashSet<usize> {
+        self.edges
+            .iter()
+            .filter_map(|(from_node, to_nodes)| {
+                if to_nodes.contains(&Edge { node_id: id }) {
+                    Some(*from_node)
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+}
+
+#[derive(Clone, Debug, Error)]
+pub enum RemoveDirectedGraphNodeError {
+    #[error("Node does not exist")]
+    NodeDoesNotExist,
+
+    #[error("Node has arcs")]
+    NodeHasArcs {
+        from: HashSet<usize>,
+        to: HashSet<usize>,
+    },
+}
+
+#[derive(Clone, Debug, Error)]
+pub enum AddArcError {
+    #[error("From node does not exist")]
+    FromNodeDoesNotExist,
+
+    #[error("To node does not exist")]
+    ToNodeDoesNotExist,
+
+    #[error("Arc already exists")]
+    ArcAlreadyExists,
+}
+
+#[derive(Clone, Debug, Error)]
+pub enum RemoveArcError {
+    #[error("From node does not exist")]
+    FromNodeDoesNotExist,
+
+    #[error("To node does not exist")]
+    ToNodeDoesNotExist,
+
+    #[error("Arc does not exist")]
+    ArcDoesNotExist,
+}
+
+pub struct DirectedGraph<'a, T> {
+    base_graph: &'a mut BaseGraph<T>,
+}
+
+impl<'a, T> Into<DirectedGraph<'a, T>> for &'a mut BaseGraph<T> {
+    fn into(self) -> DirectedGraph<'a, T> {
+        DirectedGraph { base_graph: self }
+    }
+}
+
+impl<'a, T> DirectedGraph<'a, T> {
+    pub fn remove_node(&mut self, id: usize) -> Result<(), RemoveDirectedGraphNodeError> {
+        if !self.base_graph.has_node(id) {
+            Err(RemoveDirectedGraphNodeError::NodeDoesNotExist)
+        } else {
+            let from_edges = self.base_graph.from_edges(id);
+            let to_edges = self.base_graph.to_edges(id);
+
+            if !from_edges.is_empty() || !to_edges.is_empty() {
+                Err(RemoveDirectedGraphNodeError::NodeHasArcs {
+                    from: from_edges,
+                    to: to_edges,
+                })
+            } else {
+                self.base_graph.remove_node(id);
+                Ok(())
+            }
+        }
+    }
+
+    pub fn add_arc(&mut self, from: usize, to: usize) -> Result<(), AddArcError> {
+        if !self.base_graph.has_node(from) {
+            Err(AddArcError::FromNodeDoesNotExist)
+        } else if !self.base_graph.has_node(to) {
+            Err(AddArcError::ToNodeDoesNotExist)
+        } else if self.base_graph.has_edge(from, to) {
+            Err(AddArcError::ArcAlreadyExists)
+        } else {
+            self.base_graph.add_edge(from, to);
+            Ok(())
+        }
+    }
+
+    pub fn remove_arc(&mut self, from: usize, to: usize) -> Result<(), RemoveArcError> {
+        if !self.base_graph.has_node(from) {
+            Err(RemoveArcError::FromNodeDoesNotExist)
+        } else if !self.base_graph.has_node(to) {
+            Err(RemoveArcError::ToNodeDoesNotExist)
+        } else if !self.base_graph.has_edge(from, to) {
+            Err(RemoveArcError::ArcDoesNotExist)
+        } else {
+            self.base_graph.remove_edge(from, to);
+            Ok(())
+        }
+    }
+}
+
+#[derive(Debug, Clone, Error)]
+pub enum RemoveUndirectedGraphNodeError {
+    #[error("Node does not exist")]
+    NodeDoesNotExist,
+
+    #[error("Node has ribs")]
+    NodeHasRibs(HashSet<usize>),
+}
+
+#[derive(Debug, Clone, Error)]
+pub enum AddRibError {
+    #[error("First node does not exist")]
+    FirstNodeDoesNotExist,
+
+    #[error("Second node does not exist")]
+    SecondNodeDoesNotExist,
+
+    #[error("Rib already exists")]
+    RibAlreadyExists,
+}
+
+#[derive(Debug, Clone, Error)]
+pub enum RemoveRibError {
+    #[error("First node does not exist")]
+    FirstNodeDoesNotExist,
+
+    #[error("Second node does not exist")]
+    SecondNodeDoesNotExist,
+
+    #[error("Rib does not exist")]
+    RibDoesNotExist,
+}
+
+pub struct UndirectedGraph<'a, T> {
+    base_graph: &'a mut BaseGraph<T>,
+}
+
+impl<'a, T> Into<UndirectedGraph<'a, T>> for &'a mut BaseGraph<T> {
+    fn into(self) -> UndirectedGraph<'a, T> {
+        UndirectedGraph { base_graph: self }
+    }
+}
+
+impl<'a, T> UndirectedGraph<'a, T> {
+    pub fn remove_node(&mut self, id: usize) -> Result<(), RemoveUndirectedGraphNodeError> {
+        if !self.base_graph.has_node(id) {
+            Err(RemoveUndirectedGraphNodeError::NodeDoesNotExist)
+        } else {
+            let node_ribs = self.base_graph.from_edges(id);
+            if !node_ribs.is_empty() {
+                Err(RemoveUndirectedGraphNodeError::NodeHasRibs(node_ribs))
+            } else {
+                self.base_graph.remove_node(id);
+                Ok(())
+            }
+        }
+    }
+
+    pub fn add_rib(&mut self, first: usize, second: usize) -> Result<(), AddRibError> {
+        if !self.base_graph.has_node(first) {
+            Err(AddRibError::FirstNodeDoesNotExist)
+        } else if !self.base_graph.has_node(second) {
+            Err(AddRibError::SecondNodeDoesNotExist)
+        } else if self.base_graph.has_edge(first, second) {
+            Err(AddRibError::RibAlreadyExists)
+        } else {
+            self.base_graph.add_edge(first, second);
+            self.base_graph.add_edge(second, first);
+            Ok(())
+        }
+    }
+
+    pub fn remove_rib(&mut self, first: usize, second: usize) -> Result<(), RemoveRibError> {
+        if !self.base_graph.has_node(first) {
+            Err(RemoveRibError::FirstNodeDoesNotExist)
+        } else if !self.base_graph.has_node(second) {
+            Err(RemoveRibError::SecondNodeDoesNotExist)
+        } else if !self.base_graph.has_edge(first, second) {
+            Err(RemoveRibError::RibDoesNotExist)
+        } else {
+            self.base_graph.remove_edge(first, second);
+            self.base_graph.remove_edge(second, first);
+            Ok(())
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,295 @@
+use std::io::Write;
+
+use crate::graph::{
+    AddArcError, AddRibError, BaseGraph, DirectedGraph, RemoveArcError,
+    RemoveDirectedGraphNodeError, RemoveUndirectedGraphNodeError, UndirectedGraph,
+};
+
+pub mod graph;
+
 fn main() {
-    println!("Hello, world!");
+    let directed = ask_bool("Будет ли граф ориентированным");
+    let graph: BaseGraph<i32> = BaseGraph::new();
+    command_loop(graph, directed);
+}
+
+fn ask_bool(msg: &str) -> bool {
+    loop {
+        print!("{} (yes/no): ", msg);
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        let input = input.trim().to_lowercase();
+        match input.as_str() {
+            "yes" => break true,
+            "no" => break false,
+            _ => {
+                println!("Некорректный ввод.");
+            }
+        }
+    }
+}
+
+fn command_loop(mut graph: BaseGraph<i32>, directed: bool) {
+    loop {
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let mut input = input.trim().split_whitespace();
+
+        let Some(cmd) = input.next() else {
+            eprintln!("Вы должны указать команду");
+            continue;
+        };
+
+        match cmd {
+            "print" => print_graph(&graph),
+            "clean" => {
+                graph = BaseGraph::new();
+                print_graph(&graph);
+            }
+            "add_node" => {
+                let Some(value) = input.next() else {
+                    eprintln!("Вы должны указать значение");
+                    continue;
+                };
+                let Ok(value) = value.trim().parse() else {
+                    eprintln!("Значение должно быть числом");
+                    continue;
+                };
+                graph.add_node(value);
+                print_graph(&graph);
+            }
+            "remove_node" => {
+                let Some(value) = input.next() else {
+                    eprintln!("Вы должны указать значение");
+                    continue;
+                };
+                let Ok(value) = value.trim().parse() else {
+                    eprintln!("Значение должно быть числом");
+                    continue;
+                };
+
+                if directed {
+                    let mut directed_graph: DirectedGraph<i32> = (&mut graph).into();
+                    match directed_graph.remove_node(value) {
+                        Ok(_) => print_graph(&graph),
+                        Err(e) => match e {
+                            RemoveDirectedGraphNodeError::NodeDoesNotExist => {
+                                eprintln!("Узел не найден");
+                                continue;
+                            }
+                            RemoveDirectedGraphNodeError::NodeHasArcs { from, to } => {
+                                eprintln!(
+                                    "Узел имеет входящие дуги с узлами {:?}, а также исходящие дуги с узлами {:?}",
+                                    to, from
+                                );
+
+                                if ask_bool("Удалить все дуги") {
+                                    for &node in &to {
+                                        directed_graph.remove_arc(node, value).unwrap();
+                                    }
+                                    for &node in &from {
+                                        directed_graph.remove_arc(value, node).unwrap();
+                                    }
+                                    directed_graph.remove_node(value).unwrap();
+                                    print_graph(&graph);
+                                }
+
+                                continue;
+                            }
+                        },
+                    }
+                } else {
+                    let mut undirected_graph: UndirectedGraph<i32> = (&mut graph).into();
+                    match undirected_graph.remove_node(value) {
+                        Ok(_) => print_graph(&graph),
+                        Err(e) => match e {
+                            RemoveUndirectedGraphNodeError::NodeDoesNotExist => {
+                                eprintln!("Узел не найден");
+                                continue;
+                            }
+                            RemoveUndirectedGraphNodeError::NodeHasRibs(nodes) => {
+                                eprintln!("Узел имеет рёбра с узлами {:?}", nodes);
+
+                                if ask_bool("Удалить рёбра") {
+                                    for node in nodes {
+                                        undirected_graph.remove_rib(value, node).unwrap();
+                                    }
+                                    undirected_graph.remove_node(value).unwrap();
+                                    print_graph(&graph);
+                                }
+
+                                continue;
+                            }
+                        },
+                    }
+                }
+
+                print_graph(&graph);
+            }
+            "add_arc" => {
+                if !directed {
+                    eprintln!("Граф неориентированный, добавление дуг не поддерживается");
+                    continue;
+                }
+                let Some(from) = input.next() else {
+                    eprintln!("Вы должны указать начальную вершину");
+                    continue;
+                };
+                let Ok(from) = from.trim().parse() else {
+                    eprintln!("Начальная вершина должна быть числом");
+                    continue;
+                };
+                let Some(to) = input.next() else {
+                    eprintln!("Вы должны указать конечную вершину");
+                    continue;
+                };
+                let Ok(to) = to.trim().parse() else {
+                    eprintln!("Конечная вершина должна быть числом");
+                    continue;
+                };
+
+                let mut directed_graph: DirectedGraph<i32> = (&mut graph).into();
+                match directed_graph.add_arc(from, to) {
+                    Ok(_) => print_graph(&graph),
+                    Err(e) => match e {
+                        AddArcError::FromNodeDoesNotExist => {
+                            eprintln!("Начальная вершина не существует")
+                        }
+                        AddArcError::ToNodeDoesNotExist => {
+                            eprintln!("Конечная вершина не существует")
+                        }
+                        AddArcError::ArcAlreadyExists => eprintln!("Дуга уже существует"),
+                    },
+                }
+            }
+            "add_rib" => {
+                if directed {
+                    eprintln!("Граф ориентированный, добавление рёбер не поддерживается");
+                    continue;
+                }
+                let Some(first) = input.next() else {
+                    eprintln!("Вы должны указать первую вершину");
+                    continue;
+                };
+                let Ok(first) = first.trim().parse() else {
+                    eprintln!("Первая вершина должна быть числом");
+                    continue;
+                };
+                let Some(second) = input.next() else {
+                    eprintln!("Вы должны указать вторую вершину");
+                    continue;
+                };
+                let Ok(second) = second.trim().parse() else {
+                    eprintln!("Вторая вершина должна быть числом");
+                    continue;
+                };
+                let mut undirected_graph: UndirectedGraph<i32> = (&mut graph).into();
+                match undirected_graph.add_rib(first, second) {
+                    Ok(_) => print_graph(&graph),
+                    Err(e) => match e {
+                        AddRibError::FirstNodeDoesNotExist => {
+                            eprintln!("Первая вершина не существует")
+                        }
+                        AddRibError::SecondNodeDoesNotExist => {
+                            eprintln!("Вторая вершина не существует")
+                        }
+                        AddRibError::RibAlreadyExists => eprintln!("Ребро уже существует"),
+                    },
+                }
+            }
+            "remove_arc" => {
+                if !directed {
+                    eprintln!("Граф неориентированный, удаление дуг не поддерживается");
+                    continue;
+                }
+                let Some(from) = input.next() else {
+                    eprintln!("Вы должны указать начальную вершину");
+                    continue;
+                };
+                let Ok(from) = from.trim().parse() else {
+                    eprintln!("Начальная вершина должна быть числом");
+                    continue;
+                };
+                let Some(to) = input.next() else {
+                    eprintln!("Вы должны указать конечную вершину");
+                    continue;
+                };
+                let Ok(to) = to.trim().parse() else {
+                    eprintln!("Конечная вершина должна быть числом");
+                    continue;
+                };
+                let mut directed_graph: DirectedGraph<i32> = (&mut graph).into();
+                match directed_graph.remove_arc(from, to) {
+                    Ok(_) => print_graph(&graph),
+                    Err(e) => match e {
+                        RemoveArcError::FromNodeDoesNotExist => {
+                            eprintln!("Начальная вершина не существует")
+                        }
+                        RemoveArcError::ToNodeDoesNotExist => {
+                            eprintln!("Конечная вершина не существует")
+                        }
+                        RemoveArcError::ArcDoesNotExist => {
+                            eprintln!("Дуги между вершинами не существует")
+                        }
+                    },
+                }
+            }
+            "remove_rib" => {
+                if directed {
+                    eprintln!("Граф ориентированный, удаление рёбер не поддерживается");
+                    continue;
+                }
+                let Some(first) = input.next() else {
+                    eprintln!("Вы должны указать первую вершину");
+                    continue;
+                };
+                let Ok(first) = first.trim().parse() else {
+                    eprintln!("Первая вершина должна быть числом");
+                    continue;
+                };
+                let Some(second) = input.next() else {
+                    eprintln!("Вы должны указать вторую вершину");
+                    continue;
+                };
+                let Ok(second) = second.trim().parse() else {
+                    eprintln!("Вторая вершина должна быть числом");
+                    continue;
+                };
+                let mut directed_graph: DirectedGraph<i32> = (&mut graph).into();
+                match directed_graph.remove_arc(first, second) {
+                    Ok(_) => print_graph(&graph),
+                    Err(e) => match e {
+                        RemoveArcError::FromNodeDoesNotExist => {
+                            eprintln!("Первая вершина не существует")
+                        }
+                        RemoveArcError::ToNodeDoesNotExist => {
+                            eprintln!("Вторая вершина не существует")
+                        }
+                        RemoveArcError::ArcDoesNotExist => {
+                            eprintln!("Дуги между вершинами не существует")
+                        }
+                    },
+                }
+            }
+            "exit" => {
+                println!("Good luck with that!");
+                break;
+            }
+            "" => eprintln!("Вы должны указать команду"),
+            _ => eprintln!("Неизвестная команда"),
+        }
+    }
+}
+
+fn print_graph<T>(graph: &BaseGraph<T>)
+where
+    T: std::fmt::Debug + std::hash::Hash + Eq,
+{
+    println!("{graph:#?}");
 }
